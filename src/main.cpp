42d53053c35fc15386beb0030068114b8bc394ed
Mystical433 <peterpanlee2015@gmail.com>
2025-06-14 22:31:37 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,90 +1,49 @@
-#define CommentType CommentTypeDummy
-
 #include <Geode/Geode.hpp>
 #include <Geode/modify/LevelSearchLayer.hpp>
-#include <cocos2d.h>
-
-#if defined(_WIN32)
-#include <windows.h>
-#elif defined(__APPLE__)
-#include <ApplicationServices/ApplicationServices.h>
-#endif
+#include <Geode/utils/cocos.hpp>
+#import <Cocoa/Cocoa.h>
 
 using namespace geode::prelude;
-using namespace cocos2d;
 
-class PasteHandler : public CCObject {
-public:
-    CCTextInputNode* input;
-
-    static PasteHandler* create(CCTextInputNode* input) {
-        auto ret = new PasteHandler();
-        ret->input = input;
-        ret->autorelease();
-        return ret;
-    }
-
-    void onPaste(CCObject*) {
-        input->setString(getClipboardText().c_str());
-    }
-
-    static std::string getClipboardText() {
-#if defined(_WIN32)
-        if (OpenClipboard(nullptr)) {
-            HANDLE data = GetClipboardData(CF_TEXT);
-            if (data) {
-                char* text = static_cast<char*>(GlobalLock(data));
-                if (text) {
-                    std::string result(text);
-                    GlobalUnlock(data);
-                    CloseClipboard();
-                    return result;
-                }
-            }
-            CloseClipboard();
-        }
-#elif defined(__APPLE__)
-        PasteboardRef clipboard;
-        if (PasteboardCreate(kPasteboardClipboard, &clipboard) == noErr) {
-            ItemCount count;
-            if (PasteboardGetItemCount(clipboard, &count) == noErr && count > 0) {
-                PasteboardItemID item;
-                if (PasteboardGetItemIdentifier(clipboard, 1, &item) == noErr) {
-                    CFDataRef data;
-                    if (PasteboardCopyItemFlavorData(clipboard, item, CFSTR("public.utf8-plain-text"), &data) == noErr) {
-                        const UInt8* ptr = CFDataGetBytePtr(data);
-                        CFIndex len = CFDataGetLength(data);
-                        std::string result(reinterpret_cast<const char*>(ptr), len);
-                        CFRelease(data);
-                        CFRelease(clipboard);
-                        return result;
-                    }
-                }
-            }
-            CFRelease(clipboard);
-        }
-#endif
-        return "";
-    }
-};
-
-class $modify(LevelSearchLayer) {
+class $modify(wow, LevelSearchLayer) {
     bool init(int p0) {
         if (!LevelSearchLayer::init(p0)) return false;
 
-        auto searchInput = static_cast<CCTextInputNode*>(this->getChildByID("search-input"));
+        auto searchInput = reinterpret_cast<CCTextInputNode*>(this->getChildByID("search-input"));
         if (!searchInput) return true;
 
-        auto label = CCLabelBMFont::create("Paste", "goldFont.fnt");
-        auto handler = PasteHandler::create(searchInput);
-        auto button = CCMenuItemLabel::create(label, handler, menu_selector(PasteHandler::onPaste));
-        button->setPosition({320.f, 202.f});
+        auto label = CCLabelBMFont::create("Paste", "bigFont.fnt");
+        auto pasteButton = CCMenuItemLabel::create(
+            label,
+            this,
+            menu_selector(PasteSearchMod::onPasteClicked)
+        );
+
+        pasteButton->setPosition({280.f, 20.f});
 
         auto menu = CCMenu::create();
-        menu->addChild(button);
-        menu->setPosition(CCPointZero);
-        this->addChild(menu, 100);
+        menu->addChild(pasteButton);
+        menu->setPosition({0.f, 0.f});
+        searchInput->getParent()->addChild(menu);
 
         return true;
+    }
+
+    void onPasteClicked(CCObject*) {
+        auto searchInput = reinterpret_cast<CCTextInputNode*>(this->getChildByID("search-input"));
+        if (!searchInput) return;
+
+        std::string clipboardText = getClipboardText();
+        searchInput->setString(clipboardText.c_str());
+    }
+
+    std::string getClipboardText() {
+        NSPasteboard* pasteboard = [NSPasteboard generalPasteboard];
+        NSString* content = [pasteboard stringForType:NSPasteboardTypeString];
+        if (content) {
+            return std::string([content UTF8String]);
+        } else {
+            return "";
+        }
     }
 };
